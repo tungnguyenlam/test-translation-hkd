f63b571a81701495110c4ebe3d869aa4e3d1f233
tungnguyenlam <52458895+tungnguyenlam@users.noreply.github.com>
2025-10-24 12:00:49 +0700

    Update PyTorch installation URL to cu129

--- set_up.sh
+++ set_up.sh
@@ -3,5 +3,5 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export PATH="$HOME/.cargo/bin:$PATH"
 
-uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu129
 uv pip install huggingface_hub numpy pandas datasets tokenizers torchmetrics tqdm tensorboard
